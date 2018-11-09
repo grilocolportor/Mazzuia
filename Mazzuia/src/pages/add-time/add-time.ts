8bec77a962d785a9fe230a47f6a92862cb36a6d2
grilocolportor@yahoo.com.br <grilocoportor@yahoo.com.br>
2018-11-08 22:28:53 -0200

    add crud add time

--- Mazzuia/src/pages/add-time/add-time.ts
+++ Mazzuia/src/pages/add-time/add-time.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { Partidas, PartidasProvider } from '../../providers/partidas/partidas';
 
 /**
  * Generated class for the AddTimePage page.
@@ -15,11 +16,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddTimePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  partida: Partidas;
+  key: string;
+ 
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private partidasPdvr: PartidasProvider, private toast: ToastController) {
+    if (this.navParams.data.contact && this.navParams.data.key) {
+      this.partida = this.navParams.data.contact;
+      this.key =  this.navParams.data.key;
+    } else {
+      this.partida = new Partidas();
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddTimePage');
   }
+
+  save() {
+    this.saveContact()
+      .then(() => {
+        this.toast.create({ message: 'Contato salvo.', duration: 3000, position: 'botton' }).present();
+        this.navCtrl.pop();
+      })
+      .catch(() => {
+        this.toast.create({ message: 'Erro ao salvar o contato.', duration: 3000, position: 'botton' }).present();
+      });
+  }
+ 
+  private saveContact() {
+    if (this.key) {
+      return this.partidasPdvr.update(this.key, this.partida);
+    } else {
+      return this.partidasPdvr.insert(this.partida);
+    }
+  }
+ 
 
 }
